6a86c1d49519cbf728724bd18dde5e891a5a4d3e
antiny <an@tinypulse.com>
2017-08-20 22:09:32 +0700

    fix sample 2, remove sample 1

--- app/code/Sample/HelloWorld/Block/Index/Index.php
+++ app/code/Sample/HelloWorld/Block/Index/Index.php
@@ -3,4 +3,11 @@
 class Index extends \Magento\Framework\View\Element\Template
 {
 
+  /**
+   * @return string
+   */
+  public function getHelloWorldText()
+  {
+    return 'Welcome to XinhSang.com from a Block';
+  }
 }
