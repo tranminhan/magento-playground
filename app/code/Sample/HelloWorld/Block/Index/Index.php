7739f49909bf4dc9f2b5f557ca6d0fc5a6bea0ea
antiny <an@tinypulse.com>
2017-08-20 17:02:52 +0700

    sample module 2 - working

---
+++ app/code/Sample/HelloWorld/Block/Index/Index.php
@@ -0,0 +1,6 @@
+<?php namespace Sample\HelloWorld\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ app/code/Sample/HelloWorld/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tritruongly
+ * Date: 7/9/17
+ * Time: 4:23 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Sample_HelloWorld',
+    __DIR__
+);
